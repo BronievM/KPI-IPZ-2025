4c6f5227f4e30b01ec0fd9b780b8447817631986
Broniev <bronievcorp@gmail.com>
2025-10-21 22:34:36 +0300

    21.10.'25: Upd LR3_T1 files

--- PR3/Task1/src/Main.java
+++ PR3/Task1/src/Main.java
@@ -5,13 +5,20 @@ import Composite.*;
  */
 public class Main {
     public static void main(String[] args) {
+        // Один елемент
+        System.out.println(">-- One Element:");
         GameElement tree = new SimpleElement("Tree", 2, 3);
-
+        tree.print(" ");
+        // Група з кількох простих елементів
+        System.out.println(">-- Group of simple elements:");
         CompositeElement house = new CompositeElement("House");
         house.addChild(new SimpleElement("Wall", 5, 3));
         house.addChild(new SimpleElement("Door", 1, 2));
         house.addChild(new SimpleElement("Window", 1, 1));
+        house.print(" ");
 
+        // Багаторівнева структура
+        System.out.println(">-- Many levels:");
         CompositeElement level = new CompositeElement("Game Level");
         level.addChild(tree);
         level.addChild(house);
