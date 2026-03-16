ff1f382ad5c11ff480c7a7a742fc49b59f0ee594
Broniev <bronievcorp@gmail.com>
2026-03-16 22:52:32 +0200

    [REFACTORING]: Зміна розташування файлів для зручності

---
+++ ControlWorks/MKR1/CW-T1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Class1 c1 = new Class1();
+        Class2 c2 = new Class2();
+
+        System.out.println(c1.method1());
+        System.out.println(c2.method1());
+        System.out.println(c2.method2());
+    }
+}
